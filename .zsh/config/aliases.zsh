bb2a841448353551fcb6ad00d0cc5a2cfa88d064
Miha Filej <miha@filej.net>
2013-01-08 13:48:37 +0100

    fix .G alias

--- .zsh/config/aliases.zsh
+++ .zsh/config/aliases.zsh
@@ -7,7 +7,7 @@ alias l="ls -lA"
 alias e="$EDITOR"
 
 alias g="git"
-alias .G="git --git-dir=$HOME/dotfiles.git --work-tree=$HOME/"
+alias .G="git --git-dir=$HOME/.dotfiles.git --work-tree=$HOME/"
 
 alias s="g s"
 alias r="rails"
