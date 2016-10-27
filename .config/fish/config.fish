72fd00e7c98902be8c43aaa5092c322fa48bb895
Miha Filej <miha@filej.net>
2016-10-27 10:00:37 +0200

    Skip fish greeting message on startup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 set -x EDITOR vim
 set -x HOMEBREW_CASK_OPTS "--appdir=~/Applications"
+set -e fish_greeting
 
 alias g git
 alias cask "brew cask"
