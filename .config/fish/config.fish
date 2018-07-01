ce1ad06e0dcb2283c1649845fdbe110c33a37879
Miha Filej <miha@filej.net>
2018-07-01 07:26:59 +0200

    Set full path to $EDITOR

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,12 @@
 set -g CDPATH . ~ ~/Work
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
-set -x EDITOR vim
+set -x EDITOR "/usr/local/bin/nvim"
 set -x HOMEBREW_CASK_OPTS "--appdir=~/Applications"
 set -e fish_greeting
 
 alias g git
+alias vim nvim
 alias lb "open -aLaunchBar"
 
 function .G --wraps git
