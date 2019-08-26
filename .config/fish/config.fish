783702e3cf8206c7d8dec34cefbd880ecfec8db9
Miha Filej <miha@filej.net>
2019-08-26 13:02:54 +0200

    fish: more abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,11 +5,15 @@ set -x HOMEBREW_CASK_OPTS "--appdir=~/Applications"
 set -e fish_greeting
 
 abbr --add -- - "cd -"
+abbr --add com docker-compose
+abbr --add do docker
 abbr --add g git
-abbr --add ls ls -la
+abbr --add lb open -aLaunchBar
+abbr --add pgrep pgrep -lf
+abbr --add v vim
 
+alias git hub
 alias vim nvim
-alias lb "open -aLaunchBar"
 
 function .G --wraps git
   git --git-dir=$HOME/.dotfiles.git --work-tree=$HOME/ $argv
