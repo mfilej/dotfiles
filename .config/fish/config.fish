d393e59b492a946ab8a7534d8a2b01bf13406021
Miha Filej <miha@filej.net>
2019-09-10 13:27:46 +0200

    Update fish config for keybase and gpg

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 set -x EDITOR "/usr/local/bin/nvim"
+set -x GPG_TTY (tty)
 set -x HOMEBREW_CASK_OPTS "--appdir=~/Applications"
 set -e fish_greeting
 
@@ -8,6 +9,7 @@ abbr --add -- - "cd -"
 abbr --add com docker-compose
 abbr --add do docker
 abbr --add g git
+abbr --add keybase keybase --standalone
 abbr --add lb open -aLaunchBar
 abbr --add pgrep pgrep -lf
 abbr --add v vim
