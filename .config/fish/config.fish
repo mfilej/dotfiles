6b79f911c2cf6d8d36f8641ac368a162c890e1ea
Miha Filej <miha@filej.net>
2021-08-04 16:52:15 +0200

    fish(er) updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,12 +3,14 @@ set -x LANG en_US.UTF-8
 set -x EDITOR "/usr/local/bin/nvim"
 set -x GPG_TTY (tty)
 set -x HOMEBREW_CASK_OPTS "--appdir=~/Applications"
+set -x ERL_AFLAGS "-kernel shell_history enabled -kernel shell_history_file_bytes 1024000"
 set -e fish_greeting
 
 abbr --add -- - "cd -"
 abbr --add com docker-compose
 abbr --add do docker
 abbr --add g git
+abbr --add gg gigalixir
 abbr --add keybase keybase --standalone
 abbr --add lb open -aLaunchBar
 abbr --add ls ls -lah
@@ -19,9 +21,11 @@ abbr --add v vim
 abbr --add cb  git branch
 abbr --add ce  git commit --amend --no-edit
 abbr --add co  git checkout
+abbr --add c git commit
 abbr --add crc git reset --soft HEAD^
 abbr --add cva git commit --amend -v 
 abbr --add cvc git commit -v
+abbr --add r  git rebase
 abbr --add rr  git rebase --continue
 
 abbr --add ff git pull --ff-only
