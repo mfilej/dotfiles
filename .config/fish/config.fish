3b935a54875a7038531bf3b43e707d094d6fca81
Miha Filej <miha@filej.net>
2022-07-26 10:04:51 +0200

    Set up 1Password SSH agent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ set -x GPG_TTY (tty)
 set -x HOMEBREW_CASK_OPTS "--appdir=~/Applications"
 set -x ERL_AFLAGS "-kernel shell_history enabled -kernel shell_history_file_bytes 1024000"
 set -x RIPGREP_CONFIG_PATH "$HOME/.config/ripgreprc"
+set -x SSH_AUTH_SOCK "$HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock"
 set -e fish_greeting
 
 abbr --add -- - "cd -"
