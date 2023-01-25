ae08786a0e72cc280d5fb0a1bb344bee7cf17221
Miha Filej <miha@filej.net>
2023-01-25 16:32:35 +0100

    fish: complete aws-sso harder

--- .config/fish/completions/aws-sso.fish
+++ .config/fish/completions/aws-sso.fish
@@ -1,8 +1,20 @@
-function __fish_complete_aws_sso_subcommand
-  set -l tokens (commandline -opc) (commandline -ct)
-  set -l index (contains -i -- -- (commandline -opc))
-  set -e tokens[1..$index]
-  complete -C"$tokens"
+# aws-sso completion
+function __complete_aws-sso
+    set -lx COMP_LINE (commandline -cp)
+    test -z (commandline -ct)
+    and set COMP_LINE "$COMP_LINE "
+    /opt/homebrew/bin/aws-sso
 end
 
-complete -c aws-sso -n "contains -- -- (commandline -opc)" -x -a "(__fish_complete_aws_sso_subcommand)"
+complete -f -c aws-sso -a "(__complete_aws-sso)"
+
+
+# aws-sso exec -- other-command completion
+function __fish_complete_aws_sso_subcommand
+   set -l tokens (commandline -opc) (commandline -ct)
+   set -l index (contains -i -- -- (commandline -opc))
+   set -e tokens[1..$index]
+   complete -C"$tokens"
+ end
+
+ complete -c aws-sso -n "contains -- -- (commandline -opc)" -x -a "(__fish_complete_aws_sso_subcommand)"
