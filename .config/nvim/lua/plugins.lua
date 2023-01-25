696a1363fc40da563ef4d5481d60273badca353e
Miha Filej <miha@filej.net>
2023-01-25 16:37:42 +0100

    nvim: update lsp config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -101,7 +101,7 @@ return require('packer').startup(function()
 
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities.textDocument.completion.completionItem.snippetSupport = true
-  capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+  capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
   require'lspconfig'.elixirls.setup{
     cmd = { "/opt/elixir-ls/release/elixir-ls" };
