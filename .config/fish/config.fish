c2097dbd1b48b09f77974c39e2daae1a416337a2
Miha Filej <miha@filej.net>
2023-02-18 18:54:06 +0100

    Unset HOMEBREW_INSTALL_FROM_API, now no-op

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,6 @@ set -x LANG en_US.UTF-8
 set -x EDITOR "/opt/homebrew/bin/nvim"
 set -x GPG_TTY (tty)
 set -x HOMEBREW_CASK_OPTS "--appdir=~/Applications"
-set -x HOMEBREW_INSTALL_FROM_API "true"
 set -x ERL_AFLAGS "-kernel shell_history enabled -kernel shell_history_file_bytes 1024000"
 set -x RIPGREP_CONFIG_PATH "$HOME/.config/ripgreprc"
 set -x SSH_AUTH_SOCK "$HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock"
