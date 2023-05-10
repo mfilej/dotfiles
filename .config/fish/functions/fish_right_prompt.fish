9ba1b13fa3fde3131fb832900a7a4dc1bd66826f
Miha Filej <miha@filej.net>
2023-05-10 10:43:06 +0200

    Display aws-sso account info in right prompt

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,7 +1,16 @@
 function fish_right_prompt
   set -l last_command_status $status
+
+  if test -n "$AWS_SSO_PROFILE"
+    echo "⦗"
+    set_color yellow
+    echo "$AWS_SSO_PROFILE"
+    set_color normal
+    echo "⦘"
+  end
+
   if [ $last_command_status -ne 0 ]
-    set_color 900
+    set_color -r 900
     echo "↪ $last_command_status"
     set_color normal
   end
