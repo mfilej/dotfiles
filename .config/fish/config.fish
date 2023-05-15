04bbb40d5648bbcd7ba06a3b89e0a9c5eaec0079
Miha Filej <miha@filej.net>
2023-05-15 14:14:13 +0200

    Update abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
-set -x EDITOR "/opt/homebrew/bin/nvim"
+set -x EDITOR vim
 set -x GPG_TTY (tty)
 set -x HOMEBREW_CASK_OPTS "--appdir=~/Applications"
 set -x ERL_AFLAGS "-kernel shell_history enabled -kernel shell_history_file_bytes 1024000"
@@ -20,24 +20,22 @@ abbr --add fly flyctl
 abbr --add lb open -aLaunchBar
 abbr --add ls ls -lah
 abbr --add pgrep pgrep -lf
-abbr --add v vim
+abbr --add nv nvim
 abbr --add yt yt-dlp
 abbr --add mig mix ecto.migrate
 abbr --add migen mix ecto.gen.migration
 
 # vim-fugitive equivalents
-abbr --add cb  git branch
-abbr --add ce  git commit --amend --no-edit
-abbr --add co  git checkout
+abbr --add cb git branch
+abbr --add ce git commit --amend --no-edit
+abbr --add co git checkout
 abbr --add c git commit
 abbr --add crc git reset --soft HEAD^
-abbr --add cva git commit --amend -v 
+abbr --add cva git commit --amend -v
 abbr --add cvc git commit -v
-abbr --add r  git rebase
-abbr --add rr  git rebase --continue
+abbr --add r git rebase
+abbr --add rr git rebase --continue
 
 abbr --add ff git pull --ff-only
-
-alias vim nvim
 
 source /Users/miha/.config/op/plugins.sh
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,25 +1,25 @@
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showdirtystate yes
+set __fish_git_prompt_showuntrackedfiles yes
+set __fish_git_prompt_showstashstate yes
 
-set __fish_git_prompt_char_dirtystate '𝚫'
+set __fish_git_prompt_char_dirtystate 𝚫
 set __fish_git_prompt_char_stagedstate '±'
 set __fish_git_prompt_char_untrackedfiles '+'
 set __fish_git_prompt_char_stashstate '§'
 set __fish_git_prompt_char_invalidstate '×'
 
-set __fish_git_prompt_color_dirtystate '996'
-set __fish_git_prompt_color_stagedstate '9c3'
-set __fish_git_prompt_color_untrackedfiles '999'
+set __fish_git_prompt_color_dirtystate 996
+set __fish_git_prompt_color_stagedstate 9c3
+set __fish_git_prompt_color_untrackedfiles 999
 
 function fish_prompt
-  set_color $fish_color_cwd
-  printf '%s' (prompt_pwd)
-  set_color 888
+    set_color $fish_color_cwd
+    printf '%s' (prompt_pwd)
+    set_color 888
 
-  printf '%s' (__fish_git_prompt " ⋲ %s")
+    printf '%s' (__fish_git_prompt " ⋲ %s")
 
-  set_color 444
-  printf ' ❯ '
-  set_color normal
+    set_color 444
+    printf ' > '
+    set_color normal
 end
