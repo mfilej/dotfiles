1d18da3111c0f80218e76351a4a2bfff775660ff
Miha Filej <miha@filej.net>
2023-08-05 14:50:55 +0200

    fish: udpate config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,15 @@
+# Preserve EDITOR if already set by Kitty, otherwise use vim
+set -q EDITOR || set -x EDITOR vim
+
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
-set -x EDITOR vim
+
+set -x ERL_AFLAGS "-kernel shell_history enabled -kernel shell_history_file_bytes 1024000"
 set -x GPG_TTY (tty)
 set -x HOMEBREW_CASK_OPTS "--appdir=~/Applications"
-set -x ERL_AFLAGS "-kernel shell_history enabled -kernel shell_history_file_bytes 1024000"
 set -x RIPGREP_CONFIG_PATH "$HOME/.config/ripgreprc"
 set -x SSH_AUTH_SOCK "$HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+
 set -g fish_greeting
 set -g fish_key_bindings fish_hybrid_key_bindings
 
@@ -13,17 +17,20 @@ abbr --add -- - "cd -"
 abbr --add -- ... "../.."
 abbr --add asdf rtx
 abbr --add com docker-compose
-abbr --add do docker
+abbr --add doc docker
+abbr --add fly flyctl
 abbr --add g git
 abbr --add gf gitfox
-abbr --add fly flyctl
 abbr --add lb open -aLaunchBar
 abbr --add ls ls -lah
-abbr --add pgrep pgrep -lf
-abbr --add nv nvim
-abbr --add yt yt-dlp
 abbr --add mig mix ecto.migrate
 abbr --add migen mix ecto.gen.migration
+abbr --add n nvim
+abbr --add pgrep pgrep -lf
+abbr --add yt yt-dlp
+
+# We never really want regular vim when using Kitty
+set -q KITTY_PID && abbr --add vim nvim
 
 # vim-fugitive equivalents
 abbr --add cb git branch
