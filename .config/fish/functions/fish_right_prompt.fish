d470f00b7bcf68a610affb125f7174c47406f631
Miha Filej <miha@filej.net>
2023-08-05 14:55:21 +0200

    fish: show current AWS SSO profile in right prompt

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,17 +1,17 @@
 function fish_right_prompt
-  set -l last_command_status $status
+    set -l last_command_status $status
 
-  if test -n "$AWS_SSO_PROFILE"
-    echo "⦗"
-    set_color yellow
-    echo "$AWS_SSO_PROFILE"
-    set_color normal
-    echo "⦘"
-  end
+    if test -n "$AWS_SSO_PROFILE"
+        echo "⦗"
+        set_color yellow
+        echo "$AWS_SSO_PROFILE" | cut -d ':' -f 1
+        set_color normal
+        echo "⦘"
+    end
 
-  if [ $last_command_status -ne 0 ]
-    set_color -r 900
-    echo "↪ $last_command_status"
-    set_color normal
-  end
+    if [ $last_command_status -ne 0 ]
+        set_color -r 900
+        echo "↪ $last_command_status"
+        set_color normal
+    end
 end
