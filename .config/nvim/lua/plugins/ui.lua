79fd894166ef430a8ef38d9641793f0d26ea02da
Miha Filej <miha@filej.net>
2023-08-06 21:43:52 +0200

    nvim: clean up statusline

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -17,4 +17,31 @@ return {
       },
     },
   },
+  {
+    "SmiteshP/nvim-navic",
+    enabled = false,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      local c = opts.sections.lualine_c
+      local x = opts.sections.lualine_x
+
+      -- disable navic by removing last element of sections.lualine_c
+      table.remove(c, #c)
+
+      -- move git status near the branch name
+      local lazy_status = table.remove(x, 5)
+      table.insert(c, 1, lazy_status)
+
+      -- disable the clock
+      opts.sections.lualine_z = { "searchcount" }
+
+      -- no pointy p
+      opts.options.section_separators = ""
+      opts.options.component_separators = ""
+
+      return opts
+    end,
+  },
 }
