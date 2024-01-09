cc212550994c2f0ccce86396145cfe2763ffa932
Miha Filej <miha@filej.net>
2024-01-09 10:14:36 +0100

    rtx is now mise

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,16 +9,16 @@ set -x GPG_TTY (tty)
 set -x HOMEBREW_CASK_OPTS "--appdir=~/Applications"
 set -x KERL_BUILD_DOCS yes
 set -x RIPGREP_CONFIG_PATH "$HOME/.config/ripgreprc"
-set -x SSH_AUTH_SOCK "$HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock"
 
 set -g fish_greeting
 
 # Use hybrid keybindings, except when in a terminal inside (n)vim
 set -q VIM || set -g fish_key_bindings fish_hybrid_key_bindings
 
+fish_add_path --path ~/.local/share/nvim/mason/bin /opt/homebrew/opt/ruby/bin ~/.bin
+
 abbr -g --add -- - "cd -"
 abbr -g --add -- ... "../.."
-abbr -g --add asdf rtx
 abbr -g --add com docker-compose
 abbr -g --add doc docker
 abbr -g --add fly flyctl
@@ -30,6 +30,7 @@ abbr -g --add mig mix ecto.migrate
 abbr -g --add migen mix ecto.gen.migration
 abbr -g --add n nvim
 abbr -g --add pgrep pgrep -lf
+abbr -g --add rtx mise
 abbr -g --add yt yt-dlp
 
 # We never really want regular vim when using Kitty
@@ -48,4 +49,4 @@ abbr -g --add rr git rebase --continue
 
 abbr -g --add ff git pull --ff-only
 
-source /Users/miha/.config/op/plugins.sh
+source ~/.config/op/plugins.sh
