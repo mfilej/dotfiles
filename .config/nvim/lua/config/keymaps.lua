17e39a4a84fc474efaf4aee6c3e234cf26fd7d86
Miha Filej <miha@filej.net>
2024-07-26 20:26:37 +0200

    Update keymaps for new which-key

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,45 +3,16 @@
 -- Add any additional keymaps here
 
 -- nnoremap <leader><leader> <c-^>
-local Util = require("lazyvim.util")
 local filenav = require("mf.filenav")
-local lazyterm = function(count)
-  local args = { cwd = Util.root(), border = "rounded", esc_esc = true }
-  if count ~= nil then
-    args.count = count
-  end
-  Util.terminal(nil, args)
-end
 local wk = require("which-key")
 
--- lazyterm bindings
-vim.keymap.set("t", "<c-\\>", "<cmd>close<cr>")
-wk.register({
-  ["<c-\\>"] = { lazyterm, "Terminal (root dir)" },
-  -- stylua: ignore start
-  ["<c-1>"] = { function () lazyterm(1) end, "Toggle lazyterm #1", mode = { "n", "t"}},
-  ["<c-2>"] = { function () lazyterm(2) end, "Toggle lazyterm #1", mode = { "n", "t"}},
-  ["<c-3>"] = { function () lazyterm(3) end, "Toggle lazyterm #1", mode = { "n", "t"}},
-  -- stylua: ignore end
-})
-
-wk.register({
-  ["<leader><space>"] = { "<c-^>", "Edit alternate file" },
-  ["<leader>gw"] = { "Gwrite", ":Gwrite (fugitive)" },
-  ["<C-s>"] = nil,
-  ["<S-h>"] = { "<cmd>tabp<cr>", "Previous tab" },
-  ["<S-l>"] = { "<cmd>tabn<cr>", "Next tab" },
-  ["%%"] = { "<C-R>=expand('%:h').'/'<cr>", "Current directory", mode = "c" },
-  ["[f"] = {
-    function()
-      filenav.prev_file()
-    end,
-    "Previous file in directory",
-  },
-  ["]f"] = {
-    function()
-      filenav.next_file()
-    end,
-    "Next file in directory",
-  },
+wk.add({
+  { "%%", "<C-R>=expand('%:h').'/'<cr>", desc = "Current directory", mode = "c" },
+  { "<leader><space>", "<c-^>", desc = "Edit alternate file" },
+  { "<leader>gw", "Gwrite", desc = ":Gwrite (fugitive)" },
+  { "<C-s>", nil },
+  { "<S-h>", "<cmd>tabp<cr>", desc = "Previous tab" },
+  { "<S-l>", "<cmd>tabn<cr>", desc = "Next tab" },
+  { "[f", filenav.prev_file, desc = "Previous file in directory" },
+  { "]f", filenav.next_file, desc = "Next file in directory" },
 })
