fcf057679566eba10da6c80651b5ccc72f7428d3
Miha Filej <miha@filej.net>
2025-04-09 15:54:43 +0200

    fish: tweak jj prompt

--- .config/fish/conf.d/tide.fish
+++ .config/fish/conf.d/tide.fish
@@ -24,7 +24,7 @@ set -g tide_jobs_bg_color normal
 set -g tide_jobs_color 5FAF00
 set -g tide_jobs_icon ''
 set -g tide_jobs_number_threshold 1
-set -g tide_left_prompt_items pwd shlvl private_mode newline character
+set -g tide_left_prompt_items pwd jj_op_log_id shlvl private_mode newline character
 set -g tide_private_mode_bg_color normal
 set -g tide_private_mode_color E52221
 set -g tide_private_mode_icon '󰗹'
@@ -32,7 +32,7 @@ set -g tide_prompt_transient_enabled false
 set -g tide_pwd_color_anchors C4A000
 set -g tide_pwd_color_dirs 92780C
 set -g tide_pwd_color_truncated_dirs 8787AF
-set -g tide_right_prompt_items status cmd_duration jj jj_op_log_id context jobs gcloud aws_sso_profile
+set -g tide_right_prompt_items status cmd_duration jj context jobs gcloud aws_sso_profile
 set -g tide_shlvl_bg_color normal
 set -g tide_shlvl_color green
 set -g tide_shlvl_icon ''
