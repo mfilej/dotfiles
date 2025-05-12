e906c908e50b2f7c310d6eb11db194c4bac50ff7
Miha Filej <miha@filej.net>
2025-05-12 13:12:42 +0200

    fish: update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,2 @@
-# Preserve EDITOR if already set by Kitty, otherwise use vim
-set -q EDITOR || set -x EDITOR vim
-
 # Use hybrid keybindings, except when in a terminal inside (n)vim
 set -q VIM || set -g fish_key_bindings fish_hybrid_key_bindings
